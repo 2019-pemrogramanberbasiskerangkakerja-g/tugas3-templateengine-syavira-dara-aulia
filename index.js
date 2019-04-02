ca475fd0f6bffbbd8dde6041deeed774564069e7
Syavira Tiara Zulkarnain <39144403+stiaraz@users.noreply.github.com>
2019-04-02 14:37:14 +0700

    renew member view

--- index.js
+++ index.js
@@ -13,9 +13,62 @@ app.get("/today", (req, res) => {
 
     let today = new Date();
     res.render("show_date", {now: today});
-    // let users = ({ name: "Syavira", age: 21 }, { name: "Aulia", age: 21 }, { name: "Dara", age: 21 });
+});// let users = ({ name: "Syavira", age: 21 }, { name: "Aulia", age: 21 }, { name: "Dara", age: 21 });
     // let size = 3;
     // res.render ("show_date",{users:users});
+    // var page = ('#member');
+    // var pageTemplate = page.html();
+app.get("/member", (req, res) => {
+    
+    let memberData = 
+    {
+      
+        columns : [
+        	{
+          label : "name",
+          width : "150px"
+          },
+          {
+          label : "id",
+          width : "100px"
+          },
+          {
+          label : "grade",
+          width : "100px",
+          before :  {
+              type : "bar",
+              limit : 4
+          	}
+          }
+        ],
+        
+        data: [
+            {
+                name: "Syavira",
+                id: "012",
+                grade: 4,
+                background : "blue",
+                color : "white"
+            },
+            {
+               name: "Aulia",
+                id: "012",
+                grade: 4,
+                background : "yellow",
+                color : "black"
+            },
+            {
+                name: "Dara",
+                id: "707",
+                grade: 4,
+                background : "orange",
+                color : "black"
+            }
+        ]
+    }
+;
+	res.render("member", memberData);
+
 });
 
 app.use((req, res) => {
